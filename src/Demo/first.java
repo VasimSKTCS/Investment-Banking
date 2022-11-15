af035c66ccec56202a7192806b97a7c075c9da21
SHAIKH VASIM <SHAIKH VASIM@LAPTOP-VLVVR0M3>
2022-11-15 22:49:36 +0530

    Done The Second Change

--- src/Demo/first.java
+++ src/Demo/first.java
@@ -1,18 +1,16 @@
 package Demo;
 
-public class first {//class body open
+    public class first {//class body open
 	
-
 	public static void main(String[] args) {//main method body open
 		
-		System.out.println("I Done Git Hub Sir");
-		System.out.println("Hi Java");
-		System.out.println("Jai Maharashtra");
-		System.out.println("My Nmae Is Shaikh Vasim");
-		System.out.println("Iam From Vaijapur");
-		System.out.println("Iam A Engineer");
-		// TODO Auto-generated method stub
+	System.out.println("I Done Git Hub Sir");
+	System.out.println("Hi Java");
+	System.out.println("Jai Maharashtra");
+	System.out.println("My Nmae Is Shaikh Vasim");
+	System.out.println("Iam From Vaijapur");
+	System.out.println("Iam A Engineer");
+	// TODO Auto-generated method stub
 
 	}//main method body close
-
-}//class body close
+    }//class body close
